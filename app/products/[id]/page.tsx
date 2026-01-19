f2c1aae6f9db9485cdd67c647a5c3aa790a5cdf9
Riddhik <riddhikmohite@gmail.com>
2026-01-19 12:41:46 +0530

    fixed prod erros

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -12,11 +12,11 @@ export default async function ProductDetails({ params }: Props) {
 
   return (
     <div className="max-w-4xl mx-auto p-4 text-center">
-      <Image width={100} height={80} src={product.image} alt={product.title} className="w-full h-80 object-contain" />
-      <h1 className="text-2xl font-bold mt-4">{product.title}</h1>
-      <p className="text-gray-600 mt-2">{product.category}</p>
-      <p className="mt-4">{product.description}</p>
-      <p className="mt-4 text-xl font-semibold">${product.price}</p>
+      <Image width={100} height={80} src={product?.image} alt="product image"className="w-full h-80 object-contain" />
+      <h1 className="text-2xl font-bold mt-4">{product?.title}</h1>
+      <p className="text-gray-600 mt-2">{product?.category}</p>
+      <p className="mt-4">{product?.description}</p>
+      <p className="mt-4 text-xl font-semibold">${product?.price}</p>
     </div>
   )
 }
