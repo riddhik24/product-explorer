069521878b4a159e5d8a95e99508a97679af1cc0
Riddhik <riddhikmohite@gmail.com>
2026-01-19 16:22:34 +0530

    fixed prod erros

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { getProduct } from "../../lib/api"
+import { getProduct } from "../../lib/api";
 
 interface Props {
   params: Promise<{ id: string }>;
@@ -7,20 +7,28 @@ interface Props {
 
 export default async function ProductDetails({ params }: Props) {
   const resolvedParams = await params;
-  const id = resolvedParams.id
-  const product = await getProduct(id)
+  const id = resolvedParams.id;
+  const product = await getProduct(id);
 
   return (
     <>
       {product ? (
-    <div className="max-w-4xl mx-auto p-4 text-center">
-      <Image width={100} height={80} src={product?.image} alt="product image"className="w-full h-80 object-contain" />
-      <h1 className="text-2xl font-bold mt-4">{product?.title}</h1>
-      <p className="text-gray-600 mt-2">{product?.category}</p>
-      <p className="mt-4">{product?.description}</p>
-      <p className="mt-4 text-xl font-semibold">${product?.price}</p>
-    </div>
-     ) : <h1>No Products Found</h1> }
+        <div className="max-w-4xl mx-auto p-4 text-center">
+          <Image
+            width={100}
+            height={80}
+            src={product.image}
+            alt={product.title}
+            className="w-full h-80 object-contain"
+          />
+          <h1 className="text-2xl font-bold mt-4">{product.title}</h1>
+          <p className="text-gray-600 mt-2">{product.category}</p>
+          <p className="mt-4">{product.description}</p>
+          <p className="mt-4 text-xl font-semibold">${product.price}</p>
+        </div>
+      ) : (
+        <h1>No Products Found</h1>
+      )}
     </>
-  )
+  );
 }
